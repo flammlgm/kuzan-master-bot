67dc408ae2833acdf65218e22c7baa6dc2f87eae
flammlgm <bogatovdma@gmail.com>
2026-07-05 14:18:41 +0300

    ref/new channels

--- src/youtube/channels.js
+++ src/youtube/channels.js
@@ -3,4 +3,20 @@ module.exports = [
     name: "Sneaky Dice",
     channelId: "UCe_vCsHHRPT9XPVYDInQqqQ",
   },
+  {
+    name: "Мастер Ансар",
+    channelId: "UCgANclweVtKvXL4J1ChZPwQ",
+  },
+  {
+    name: "Грядут Приключения | Новый канал",
+    channelId: "UCnOKoB5dGqQ3vXVMLj5t5Pg",
+  },
+  {
+    name: "Kasoda",
+    channelId: "UCZ4HClc1DGxY016-2HmT73w",
+  },
+  {
+    name: "GM Marty",
+    channelId: "UCVABVTJdfR9JpVdDqyslEOQ",
+  },
 ];
